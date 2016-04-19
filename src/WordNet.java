233b682fd34b4e10744cb3507a396b925124cfff
Josh Brenden <joshuabrenden@gmail.com>
2016-04-18 20:57:29 -0600

    Initial Commit.

---
+++ src/WordNet.java
@@ -0,0 +1,34 @@
+
+public class WordNet {
+
+	   // constructor takes the name of the two input files
+	   public WordNet(String synsets, String hypernyms){
+		   
+	   }
+
+	   // all WordNet nouns
+	   public Iterable<String> nouns(){
+		   return null;
+	   }
+
+	   // is the word a WordNet noun?
+	   public boolean isNoun(String word){
+		   return true;
+	   }
+
+	   // a synset (second field of synsets.txt) that is a shortest common ancestor
+	   // of noun1 and noun2 (defined below)
+	   public String sca(String noun1, String noun2){
+		   return null;
+	   }
+
+	   // distance between noun1 and noun2 (defined below)
+	   public int distance(String noun1, String noun2){
+		   return 0;
+	   }
+
+	   // do unit testing of this class
+	   public static void main(String[] args){
+		   
+	   }
+}
