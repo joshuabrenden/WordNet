dcca5ae22dc7b6729ec7b8095d34c7e647817c52
Josh Brenden <joshuabrenden@gmail.com>
2016-04-21 22:26:50 -0600

    Began synset parsing function.

--- src/ShortestCommonAncestor.java
+++ src/ShortestCommonAncestor.java
@@ -3,43 +3,52 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+/* An ancestral path between two vertices v and w in a rooted DAG
+ * is a directed path from v to a common ancestor x, 
+ * together with a directed path from w to the same ancestor x. 
+ * A shortest ancestral path is an ancestral path of minimum total length. 
+ * We refer to the common ancestor in a shortest ancestral path as a shortest common ancestor. 
+ * Note that a shortest common ancestor always exists because the root is an ancestor of every vertex.
+ * Note also that an ancestral path is a path, but not a directed path.
+ * */
+
 public class ShortestCommonAncestor {
 	// constructor takes a rooted DAG as argument
-	public ShortestCommonAncestor(Digraph G){
-		
+	public ShortestCommonAncestor(Digraph G) {
+
 	}
 
-	   // length of shortest ancestral path between v and w
-	public int length(int v, int w){
+	// length of shortest ancestral path between v and w
+	public int length(int v, int w) {
 		return 0;
 	}
 
-	   // a shortest common ancestor of vertices v and w
-	public int ancestor(int v, int w){
+	// a shortest common ancestor of vertices v and w
+	public int ancestor(int v, int w) {
 		return 0;
 	}
 
-	   // length of shortest ancestral path of vertex subsets A and B
-	public int length(Iterable<Integer> subsetA, Iterable<Integer> subsetB){
+	// length of shortest ancestral path of vertex subsets A and B
+	public int length(Iterable<Integer> subsetA, Iterable<Integer> subsetB) {
 		return 0;
 	}
 
-	   // a shortest common ancestor of vertex subsets A and B
-	public int ancestor(Iterable<Integer> subsetA, Iterable<Integer> subsetB){
+	// a shortest common ancestor of vertex subsets A and B
+	public int ancestor(Iterable<Integer> subsetA, Iterable<Integer> subsetB) {
 		return 0;
 	}
 
-	   // do unit testing of this class
-	public static void main(String[] args){
+	// do unit testing of this class
+	public static void main(String[] args) {
 		In in = new In(args[0]);
-	    Digraph G = new Digraph(in);
-	    ShortestCommonAncestor sca = new ShortestCommonAncestor(G);
-	    while (!StdIn.isEmpty()) {
-	        int v = StdIn.readInt();
-	        int w = StdIn.readInt();
-	        int length   = sca.length(v, w);
-	        int ancestor = sca.ancestor(v, w);
-	        StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
-	    }
+		Digraph G = new Digraph(in);
+		ShortestCommonAncestor sca = new ShortestCommonAncestor(G);
+		while (!StdIn.isEmpty()) {
+			int v = StdIn.readInt();
+			int w = StdIn.readInt();
+			int length = sca.length(v, w);
+			int ancestor = sca.ancestor(v, w);
+			StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+		}
 	}
 }
--- src/WordNet.java
+++ src/WordNet.java
@@ -1,36 +1,80 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdOut;
 
 public class WordNet {
 
-	   // constructor takes the name of the two input files
-	   public WordNet(String synsets, String hypernyms){
-		   
-	   }
+    private final Map<Integer, String> synsetIds;
+    private final Map<String, Set<Integer>> nounIds;
+	
+	// constructor takes the name of the two input files
+	public WordNet(String synsets, String hypernyms) {
+		synsetIds = new HashMap<Integer,String>();
+		nounIds = new HashMap<String, Set<Integer>>();
+		
+		
+		readSynsets(synsets);
+	}
 
-	   // all WordNet nouns
-	   public Iterable<String> nouns(){
-		   return null;
-	   }
+	// all WordNet nouns
+	public Iterable<String> nouns() {
+		return null;
+	}
 
-	   // is the word a WordNet noun?
-	   public boolean isNoun(String word){
-		   return true;
-	   }
+	/*
+	 * Is the word a WordNet noun? Should run in time logarithmic (or better) in
+	 * the number of nouns.
+	 */
+	public boolean isNoun(String word) {
+		return true;
+	}
 
-	   // a synset (second field of synsets.txt) that is a shortest common ancestor
-	   // of noun1 and noun2 (defined below)
-	   public String sca(String noun1, String noun2){
-		   return null;
-	   }
+	/*
+	 * A synset (second field of synsets.txt) that is a shortest common ancestor
+	 * of noun1 and noun2. should make exactly one call to the length() and
+	 * ancestor().
+	 */
+	public String sca(String noun1, String noun2) {
+		return null;
+	}
 
-	   // distance between noun1 and noun2 (defined below)
-	   public int distance(String noun1, String noun2){
-		   return 0;
-	   }
+	/*
+	 * distance between noun1 and noun2. should make exactly one call to the
+	 * length().
+	 */
+	public int distance(String noun1, String noun2) {
+		return 0;
+	}
 
-	   // do unit testing of this class
-	   public static void main(String[] args){
-		   
-	   }
+	public Digraph readHypernyms(String hypernyms) {
+
+		return null;
+	}
+
+	private int readSynsets(String synsets) {
+		In synsetsFile = new In(synsets);
+		
+		//Might be over-complicating this.
+		List<String> allLines = new ArrayList<String>(Arrays.asList(synsetsFile.readAllLines()));
+
+		allLines.stream() //
+				.forEach(line -> synsetIds.put(Integer.valueOf(line.split(",")[0]), line.split(",")[1]));
+		
+		for(int i = 0; i < synsetIds.size(); i++){
+			System.out.println(synsetIds.get(i).toString());
+		}
+		
+		return 0;
+	}
+
+	// do unit testing of this class
+	public static void main(String[] args) {
+		WordNet wordNet = new WordNet(args[0], args[1]);
+	}
 }
